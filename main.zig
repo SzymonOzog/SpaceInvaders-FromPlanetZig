767a97ca10b9bb168d4bcf01e2ba4ded9f941580
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2024-02-10 14:15:45 +0100

    remove deprecated code

--- main.zig
+++ main.zig
@@ -1,37 +1,6 @@
 const std = @import("std");
-const windows = std.os.windows;
-const kernel32 = windows.kernel32;
 const ds = @import("data_structures.zig");
 const w = @import("windows_window.zig");
-
-const COORD = windows.COORD;
-const DWORD = windows.DWORD;
-const HANDLE = windows.HANDLE;
-
-pub fn clearConsole() !void {
-    var csbi: windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
-    std.debug.print("\nterm size {d}, {d}!\n", .{ csbi.dwSize.X, csbi.dwSize.Y });
-    const optional_stdoutHandle: ?HANDLE = kernel32.GetStdHandle(windows.STD_OUTPUT_HANDLE);
-    if (optional_stdoutHandle) |stdoutHandle| {
-        if (kernel32.GetConsoleScreenBufferInfo(stdoutHandle, &csbi) == 0) {
-            std.debug.print("Error", .{});
-        }
-        const tl: COORD = .{ .X = 0, .Y = 0 };
-        var written: DWORD = undefined;
-        std.debug.print("\nterm size {d}, {d}!\n", .{ csbi.dwSize.X, csbi.dwSize.Y });
-        const consoleX: u32 = @intCast(csbi.dwSize.X);
-        const consoleY: u32 = @intCast(csbi.dwSize.Y);
-        const cells = consoleX * consoleY;
-
-        if (kernel32.FillConsoleOutputCharacterA(stdoutHandle, ' ', cells, tl, &written) == 0) {
-            std.debug.print("Error at FillConsoleOutputCharacterA", .{});
-        } else if (kernel32.FillConsoleOutputAttribute(stdoutHandle, csbi.wAttributes, cells, tl, &written) == 0) {
-            std.debug.print("Error at FillConsoleOutputAttribute", .{});
-        } else if (kernel32.SetConsoleCursorPosition(stdoutHandle, tl) == 0) {
-            std.debug.print("Error at SetConsoleCursorPosition", .{});
-        }
-    }
-}
 
 const blockSize = 30;
 const blockOffset = 5;
@@ -205,7 +174,6 @@ pub fn areColliding(o1: ds.Object, o2: ds.Object) bool {
 }
 
 pub fn main() void {
-    try clearConsole();
     w.createWindow(W, H, &buffer);
     var deltaTime: f32 = 0;
     var enemyGoingLeft = false;
